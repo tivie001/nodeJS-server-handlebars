03d872bbea2b2dc560e114273ff77c162ceeb2b4
Tyler Ivie <tylerivie@MacBook-Pro.local>
2020-10-04 22:33:42 -0600

    Got handlebars to render 3 cards from 3 harry potter characters.

--- index.js
+++ index.js
@@ -1,62 +1,50 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
+fakeAPI = () => {
+    return [
+        {
+            "_id": "5a12292a0f5ae10021650d7e",
+            "name": "Harry Potter",
+            "role": "student",
+            "house": "Gryffindor",
+            "school": "Hogwarts School of Witchcraft and Wizardry",
+            "alias": "The Boy Who Lived",
+            "wand": "Holly, 11\", phoenix feather",
+            "boggart": "dementor",
+            "patronus": "stag",
+            "image": "https://www.pngitem.com/pimgs/m/13-135859_harry-potter-hd-png-download.png"
+        },
+        {
+            "_id": "5a122c200f5ae10021650d88",
+            "name": "Tom Riddle",
+            "house": "Slytherin",
+            "school": "Hogwarts School of Witchcraft and Wizardry",
+            "alias": "Lord Voldemort",
+            "wand": "Yew, 13 1/2\", phoenix feather core",
+            "boggart": "his own corpse",
+            "image": "https://media.harrypotterfanzone.com/teenage-tom-riddle-half-blood-prince-portrait.jpg"
+        },
+        {
+            "_id": "5a12327c0f5ae10021650d94",
+            "name": "Salazar Slytherin",
+            "role": "Founder, Hogwarts",
+            "house": "Slytherin",
+            "school": "Hogwarts School of Witchcraft and Wizardry",
+            "image": "https://vignette.wikia.nocookie.net/harrypotter/images/6/60/PM_SalazarSlytherin_Founders.jpg/revision/latest/top-crop/width/360/height/450?cb=20180611201037"
+        }
+    ];
+}
 
-const characters = [
-    {
-        "_id": "5a12292a0f5ae10021650d7e",
-        "name": "Harry Potter",
-        "role": "student",
-        "house": "Gryffindor",
-        "school": "Hogwarts School of Witchcraft and Wizardry",
-        "alias": "The Boy Who Lived",
-        "wand": "Holly, 11\", phoenix feather",
-        "boggart": "dementor",
-        "patronus": "stag",
-    },
-    {
-        "_id": "5a122c200f5ae10021650d88",
-        "name": "Tom Riddle",
-        "house": "Slytherin",
-        "school": "Hogwarts School of Witchcraft and Wizardry",
-        "alias": "Lord Voldemort",
-        "wand": "Yew, 13 1/2\", phoenix feather core",
-        "boggart": "his own corpse",
-    },
-    {
-        "_id": "5a12327c0f5ae10021650d94",
-        "name": "Salazar Slytherin",
-        "role": "Founder, Hogwarts",
-        "house": "Slytherin",
-        "school": "Hogwarts School of Witchcraft and Wizardry",
-    }
-]
-app.get("/api/characters", (req, res, next) => {
-    res.send(characters)
-    next()
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+}));
+app.use(express.static('public'));
+app.get("/", (req, res) => {
+    res.render('main', { layout: 'index', characters: fakeAPI()});
 })
-app.post('/api/characters/new', (req, res) => {
-    characters.push({
-        "_id": "5a0fa7dcae5bc100213c2338",
-        "name": "Sirius Black",
-        "house": "Gryffindor",
-        "school": "Hogwarts School of Witchcraft and Wizardry",
-        "alias": "Padfoot",
-        "animagus": "black dog"
-    })
-    res.send("Character Added!");
-});
-app.put('/api/characters/update/:id', (req, res) => {
-    const id = req.params.id;
-    const updatedIndex = characters.findIndex(element => element._id === id);
-    characters[updatedIndex] = {...characters[updatedIndex], name: "Tyler Ivie"}
-    res.send("Character Updated!");
-});
-app.delete('/api/characters/delete/:id', (req, res) => {
-    const id = req.params.id;
-    const updatedIndex = characters.findIndex(element => element._id === id);
-    characters[updatedIndex] = {}
-    res.send("Character Deleted!");
-})
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server running on port 3000");
+app.listen(process.env.PORT || 3002, () => {
+    console.log("Server running on port 3002");
 });
